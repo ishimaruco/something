1a21df743688892351e5975190559377e01ae8b1
ishimarut <ishimarut@notespace.jp>
2021-12-24 20:34:26 +0900

    update: なんか色々してる

---
+++ 0cal/types/vue.d.ts
@@ -0,0 +1,51 @@
+// import { NuxtCookies } from 'cookie-universal-nuxt'
+// import type VueI18n from 'vue-i18n'
+// import { ReCaptchaInstance } from '@nuxtjs/recaptcha'
+// import { accessorType } from '~/store'
+
+// declare module '@nuxt/types' {
+//     interface Context {
+//         $cookies: NuxtCookies
+//         $accessor: typeof accessorType
+//         $recaptcha: ReCaptchaInstance
+//         i18n: VueI18n
+//         localeGetObj: (obj: unknown, name: string) => string
+//         $t: typeof VueI18n.prototype.t
+//         $tc: typeof VueI18n.prototype.tc
+//         $te: typeof VueI18n.prototype.te
+//         $d: typeof VueI18n.prototype.d
+//         $n: typeof VueI18n.prototype.n
+//     }
+
+//     interface NuxtAppOptions {
+//         $cookies: NuxtCookies
+//         $accessor: typeof accessorType
+//         $recaptcha: ReCaptchaInstance
+//         i18n: VueI18n
+//         localeGetObj: (obj: unknown, name: string) => string
+//     }
+// }
+
+// declare module 'vue/types/vue' {
+//     interface Vue {
+//         $cookies: NuxtCookies
+//         $accessor: typeof accessorType
+//         $recaptcha: ReCaptchaInstance
+//         i18n: VueI18n
+//         localeGetObj: (obj: unknown, name: string) => string
+//     }
+// }
+
+// declare module 'vuex/types/index' {
+//     interface Store {
+//         $cookies: NuxtCookies
+//         $accessor: typeof accessorType
+//         i18n: VueI18n
+//         localeGetObj: (obj: unknown, name: string) => string
+//         $t: typeof VueI18n.prototype.t
+//         $tc: typeof VueI18n.prototype.tc
+//         $te: typeof VueI18n.prototype.te
+//         $d: typeof VueI18n.prototype.d
+//         $n: typeof VueI18n.prototype.n
+//     }
+// }
